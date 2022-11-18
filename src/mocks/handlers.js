d0e0b1fd9c3a414ea7ea4a4aafabc786655a0548
Kishore Kumar Andra <50901558+kishoreandra@users.noreply.github.com>
2022-11-18 13:34:54 +0530

    mock service worker setup

---
+++ src/mocks/handlers.js
@@ -0,0 +1,20 @@
+// src/mocks/handlers.js
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          name: "Mint chip",
+          imagePath: "/images/mint-chip.png",
+        },
+        {
+          name: "Vanilla",
+          imagePath: "/images/vanilla.png",
+        },
+      ])
+    );
+  }),
+];
